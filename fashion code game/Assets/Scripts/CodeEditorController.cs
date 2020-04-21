9322942497871e87aaaecf2e59b828bf629a6ba2
Boyd Smit <boydsmit@live.nl>
2020-04-21 15:48:38 +0200

    Added a prototype for the code editor

---
+++ fashion code game/Assets/Scripts/CodeEditorController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class CodeEditorController : MonoBehaviour
+{
+    private InputField _inputField;
+
+    private void Start()
+    {
+        _inputField = gameObject.GetComponent<InputField>();
+    }
+
+    private void OnButtonClick()
+    {
+        string[] writtenCode = _inputField.text.Split('\n');
+        string[] test = {"Hi", "Hello"};
+    for (int i = 0; i < writtenCode.Length; i++)
+        {
+            if (writtenCode[i] == test[i])
+            {
+                print("Correct!");
+            }
+        }
+    }
+ 
+}
