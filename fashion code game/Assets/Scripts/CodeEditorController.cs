80a8fb6455bc933a0b9e68915826b591811467e3
Boyd Smit <boydsmit@live.nl>
2020-05-11 15:28:51 +0200

    Added Newtonsoft json

--- fashion code game/Assets/Scripts/CodeEditorController.cs
+++ fashion code game/Assets/Scripts/CodeEditorController.cs
@@ -1,26 +1,48 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
+using UnityEngine;
 using UnityEngine.UI;
 
 public class CodeEditorController : MonoBehaviour
 {
     private InputField _inputField;
+    private List<CommandInfo> _commandInfoList;
 
     private void Start()
     {
+        using (StreamReader r = new StreamReader(@"Assets\Scripts\Commands\Commands.json"))
+        {
+            string json = r.ReadToEnd();
+            print(json);
+            _commandInfoList = JsonConvert.DeserializeObject<List<CommandInfo>>(json);
+        }
         _inputField = gameObject.GetComponent<InputField>();
     }
 
-    private void OnButtonClick()
+    public void OnButtonClick()
     {
         string[] writtenCode = _inputField.text.Split('\n');
-        string[] test = {"Hi", "Hello"};
-    for (int i = 0; i < writtenCode.Length; i++)
+        string[] codeSpaghettiMode = {"Neck.Sew"};
+        string[] optionsSpaghettiMode = {"Red", "Green", "Blue"};
+
+        // XmlReader xmlReader = new XmlReader();
+        //
+        // xmlReader.CreateCommandInfo(@"Assets\Scripts\Commands\Commands.xml", "command");
+        
+        for (int i = 0; i < writtenCode.Length; i++)
         {
-            if (writtenCode[i] == test[i])
+            if (writtenCode[i].Contains(codeSpaghettiMode[i] + '('))
             {
-                print("Correct!");
+                string parameter = writtenCode[i].Replace(codeSpaghettiMode[i] + "(", "");
+                foreach (var option in optionsSpaghettiMode)
+                {
+                    if (option == parameter.Replace(")", ""))
+                    {
+                        print("The parameter was: " + option);
+                    }    
+                }
             }
         }
     }
- 
 }
