918072f0146740a850b1ac92c3add32a16a45b77
Boyd Smit <boydsmit@live.nl>
2020-05-19 14:59:16 +0200

    Added Clothing Preset class. Added prototype of PresetSaver\

---
+++ fashion code game/Assets/Scripts/Clothing/ClothingPresets/ClothingPreset.cs
@@ -0,0 +1,31 @@
+﻿namespace Clothing.ClothingPresets
+{
+    public class ClothingPreset
+    {
+        private static string _clothingBase;
+        private static string _length;
+        private static string _color;
+
+        public ClothingPreset(string clothingBase, string length, string color)
+        {
+            _clothingBase = clothingBase;
+            _length = length;
+            _color = color;
+        }
+
+        public string GetClothingBase()
+        {
+            return _clothingBase;
+        }
+        
+        public string GetLength()
+        {
+            return _length;
+        }
+
+        public string GetColor()
+        {
+            return _color;
+        }
+    }
+}
---
+++ fashion code game/Assets/Scripts/Clothing/ClothingPresets/PresetSaver.cs
@@ -0,0 +1,32 @@
+﻿using System.IO;
+using Newtonsoft.Json;
+using UnityEngine;
+
+
+namespace Clothing.ClothingPresets
+{
+    public class PresetSaver : MonoBehaviour
+    {
+        private void GetPresetValues()
+        {
+
+        }
+
+        private void WriteJsonFile(string clothingBase, string length, string color)
+        {
+            using (var streamWriter = File.CreateText(@""))
+            using (var jsonWriter = new JsonTextWriter(streamWriter))
+            {
+                jsonWriter.WriteStartObject();
+                jsonWriter.WritePropertyName("Base");
+                jsonWriter.WriteValue(clothingBase);
+                jsonWriter.WritePropertyName("Length");
+                jsonWriter.WriteValue(length);
+                jsonWriter.WritePropertyName("Color");
+                jsonWriter.WriteValue(color);
+                jsonWriter.WriteEnd();
+                jsonWriter.WriteEndObject();
+            }
+        }
+    }
+}
